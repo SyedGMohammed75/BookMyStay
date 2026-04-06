4272193f6da709886f51c11fdd2051e4aeedd44f
SyedGMohammed75 <psychc75@gmail.com>
2026-04-07 01:09:02 +0530

    UC12: Data Persistence Recovery

---
+++ src/UC12DataPersistenceRecovery.java
@@ -0,0 +1,23 @@
+public class UC12DataPersistenceRecovery {
+
+    public static void main(String[] args) {
+
+        System.out.println("System Recovery");
+
+        String filePath = "inventory.txt";
+
+        RoomInventory inventory = new RoomInventory();
+        FilePersistenceService persistence = new FilePersistenceService();
+
+        persistence.loadInventory(inventory, filePath);
+
+        System.out.println("\nCurrent Inventory:");
+        System.out.println("Single: " + inventory.getAvailability("Single"));
+        System.out.println("Double: " + inventory.getAvailability("Double"));
+        System.out.println("Suite: " + inventory.getAvailability("Suite"));
+
+        persistence.saveInventory(inventory, filePath);
+
+        System.out.println("Inventory saved successfully.");
+    }
+}
